63a72702bef1b33ab92427aa9799e8b30fe1a010
Islam Abdallah <iabdallah@Office.Kashier.io>
2025-11-04 20:56:29 +0200

    Refactor The Config File Report Key

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'always' }]],
+  //reporter: [['html', { open: 'always' }]], // For Local RUn
+  reporter: [['html', { open: 'never' }]],  //For The Ci Pipeline Run
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   expect:
   {
